bf155845d244a327029557d2e36975416a7092c6
Anamika <anamikadobriyal21@gmail.com>
2025-12-26 20:33:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-
-import React from "react";
+import { useEffect, useState } from "react";
 import './../styles/App.css';
 
-const App = () => {
+function App() {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h2>You've been on this page for {seconds} seconds.</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
